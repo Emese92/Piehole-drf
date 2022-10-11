fcf066832731c7c9fdb4cea68149d5892ad72bea
Emese92 <gregor.emese@gmail.com>
2022-10-11 16:47:49 +0000

    Add profile permissions and is_owner field to ProfileSerializer

---
+++ piehole_drf/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+# This permission was created based on the example project of Code Institute
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 from .models import Profile
 
+# This serializer was created based on the example project of Code Institute
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
-        fields = '__all__'
-
-# ['id', 'owner', 'created_at','updated_at', 'name', 'content', 'image']
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at',
+            'name', 'content', 'image', 'is_owner',
+            ]
