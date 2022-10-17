ebe4fbadde96cf7b6fceb80b403e9e81bbba73eb
Emese92 <gregor.emese@gmail.com>
2022-10-17 20:29:31 +0000

    Add data filtering to recipeSerializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Recipe
+from likes.models import Like
+from bookmarks.models import Bookmark
 
 # This Serializer was created based on the example project of Code Institute
 
@@ -9,6 +11,8 @@ class RecipeSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
+    bookmark_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -29,11 +33,29 @@ class RecipeSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, recipe=obj
+            ).first()
+            return like.id if like else None
+        return None
+
+    def get_bookmark_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            bookmark = Bookmark.objects.filter(
+                owner=user, recipe=obj
+            ).first()
+            return bookmark.id if bookmark else None
+        return None
+
     class Meta:
         model = Recipe
         fields = [
             'id', 'owner', 'created_at', 'updated_at',
             'title', 'prep_time', 'image', 'is_owner', 'number_of_portions',
             'ingredients', 'steps', 'profile_id',
-            'profile_image'
+            'profile_image', 'like_id', 'bookmark_id',
             ]
