4a6d4bcdb79d200db11a525819c2dc9592c62f25
Emese92 <gregor.emese@gmail.com>
2022-10-25 01:40:44 +0000

    Add filter to comments and redeploy

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from piehole_drf.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
+
 
 # This view was created based on the example project of Code Institute
 
@@ -12,6 +14,8 @@ class CommentList(generics.ListCreateAPIView):
         permissions.IsAuthenticatedOrReadOnly
     ]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['recipe']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
